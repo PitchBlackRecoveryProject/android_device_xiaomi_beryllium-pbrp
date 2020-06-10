09e5bcf6e5ea68ee46fee91cca95456eec1d1113
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-06-10 19:15:13 +0700

    beryllium: Add copyright

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright 2018 The Android Open Source Project
+# Copyright 2020 PitchBlackRecoveryProject
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
